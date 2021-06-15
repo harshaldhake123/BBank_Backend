a191cc89177e88af7b9dcb916c1e473cfd0d25c2
Yash <yashgandhi2020@gmail.com>
2021-06-15 15:04:20 +0530

    added heroku database

--- connection.js
+++ connection.js
@@ -4,34 +4,12 @@ dotenv.config();
 
 //localhost
 
-try {
-	var conn = mysql.createConnection({
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.MYSQL_DB,
-	});
-
-	conn.connect((err) => {
-		if (!err) {
-			console.log("Database is connected successfully !");
-		} else {
-			console.log("Connection Failed !" + err.message);
-		}
-	});
-} catch (e) {
-	console.dir(e);
-}
-
-
-//heroku
-
 // try {
 // 	var conn = mysql.createConnection({
-// 		host: "us-cdbr-east-04.cleardb.com",
-// 		user: "b37f9a855061b6",
-// 		password: "b83e3b8e",
-// 		database: "heroku_71fd201f7b7d076",
+// 		host: process.env.DB_HOST,
+// 		user: process.env.DB_USER,
+// 		password: process.env.DB_PASSWORD,
+// 		database: process.env.MYSQL_DB,
 // 	});
 
 // 	conn.connect((err) => {
@@ -44,6 +22,28 @@ try {
 // } catch (e) {
 // 	console.dir(e);
 // }
+
+
+//heroku
+
+try {
+	var conn = mysql.createConnection({
+		host: "us-cdbr-east-04.cleardb.com",
+		user: "b37f9a855061b6",
+		password: "b83e3b8e",
+		database: "heroku_71fd201f7b7d076",
+	});
+
+	conn.connect((err) => {
+		if (!err) {
+			console.log("Database is connected successfully !");
+		} else {
+			console.log("Connection Failed !" + err.message);
+		}
+	});
+} catch (e) {
+	console.dir(e);
+}
 
 
 
